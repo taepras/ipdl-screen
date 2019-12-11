22cd0aeedb1fb01284e81f0f89528a883a400cfd
Tae Prasongpongchai <taepras@gmail.com>
2019-12-11 18:40:04 -0500

    start in fullscreen

--- start.bat
+++ start.bat
@@ -1,3 +1,7 @@
 start cmd /k nodemon app.js
 sleep 15
-start "" "http://localhost:3000"
+@echo off
+echo Countdown to application launch...
+timeout /t 10
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --chrome --kiosk http://localhost:3000 --disable-pinch --no-user-gesture-required
+exit
