1d6d707be751492c188bf4c974ed132627ec3dfe
Tae Prasongpongchai <taepras@gmail.com>
2020-03-09 16:50:04 -0400

    fix restore page bubble

--- start.bat
+++ start.bat
@@ -3,5 +3,5 @@ sleep 15
 @echo off
 echo Countdown to application launch...
 timeout /t 10
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --chrome --kiosk http://localhost:3000 --disable-pinch --no-user-gesture-required
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --chrome --kiosk --incognito http://localhost:3000 --disable-pinch --no-user-gesture-required
 exit
